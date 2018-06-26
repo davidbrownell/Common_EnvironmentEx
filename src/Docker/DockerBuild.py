c43cca1be9ec3a4b5e36fa4fd4e3a2892ed3c694
David Brownell <db@DavidBrownell.com>
2018-06-25 20:24:04 -0700

    Added DockerBuild.py

---
+++ src/Docker/DockerBuild.py
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------
+# |  
+# |  DockerBuild.py
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2018-06-25 20:12:13
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2018.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+# """Builds the Common_EnvironmentEx Docker image"""
+
+import os
+import sys
+
+from CommonEnvironment import BuildImpl
+from CommonEnvironment.BuildImpl import DockerBuildImpl
+from CommonEnvironment import CommandLine
+
+# ----------------------------------------------------------------------
+_script_fullpath = os.path.abspath(__file__) if "python" in sys.executable.lower() else sys.executable
+_script_dir, _script_name = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+APPLICATION_NAME                            = "Docker_CommonEnvironmentEx"
+
+Build                                       = DockerBuildImpl.CreateRepositoryBuildFunc( "Common_EnvironmentEx",
+                                                                                         os.path.join(_script_dir, "..", ".."),
+                                                                                         "dbrownell",
+                                                                                         "common_environmentex",
+                                                                                         "dbrownell/common_environment:base",
+                                                                                         "David Brownell <db@DavidBrownell.com>",
+                                                                                         repository_source_excludes=[],
+                                                                                         repository_activation_configurations=[ "python36",
+                                                                                                                                "python27",
+                                                                                                                              ],
+                                                                                       )
+
+Clean                                       = DockerBuildImpl.CreateRepositoryCleanFunc()
+
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+    try:
+        sys.exit(BuildImpl.Main(BuildImpl.Configuration( name=APPLICATION_NAME,
+                                                         requires_output_dir=False,
+                                                       )))
+    except KeyboardInterrupt:
+        pass
