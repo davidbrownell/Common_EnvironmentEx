f848ea16f28aba277e97c18976f1a3114bd646e7
David Brownell <db@DavidBrownell.com>
2018-08-20 21:53:58 -0700

    Updated Package.py

--- Libraries/Python/CommonEnvironmentEx/v1.0/CommonEnvironmentEx/Package.py
+++ Libraries/Python/CommonEnvironmentEx/v1.0/CommonEnvironmentEx/Package.py
@@ -24,6 +24,8 @@ import os
 import sys
 import traceback
 
+import six
+
 from contextlib import contextmanager
 
 from CommonEnvironment.CallOnExit import CallOnExit
@@ -34,51 +36,35 @@ _script_dir, _script_name = os.path.split(_script_fullpath)
 # ----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
-def CreatePackageName(package, name, file):
+@contextmanager
+def InitRelativeImports():
     """\
-    Creates a package name.
+    Temporarily applies a new package name to facilitate relative imports.
 
-    Usage:
-        __package__ = CreatePackageName(__package__, __name__, __file__)
+    Example:
+        with InitRelativeImports():
+            from .Here.Is.A.Relative.Import import AFile
     """
 
-    if package:
-        return package
+    frame = inspect.stack()[2][0]           # 1 for ctor, 1 for contextmanager
+    mod = inspect.getmodule(frame)
 
-    name_parts = name.split('.')
-    if len(name_parts) > 1:
-        # If the name already has dots, it means that the current module
-        # is part of an actual package and we can use it directly.
-        #
-        # By default, the name includes the current filename. However, the
-        # filename shouldn't be a part of the returned package name.
-        name = '.'.join(name_parts[:-1])
-        assert name in sys.modules, name
+    temporary_modules = {}
 
-        return name
-
-    # Ensure that relative imports work as expected by inserting the current dir
-    # at the head of the path.
-    original_dir = os.path.dirname(os.path.realpath(file))
-
-    if original_dir in sys.path:
-        sys.path.remove(original_dir)
-
-    sys.path.insert(0, original_dir)
-    with CallOnExit(lambda: sys.path.pop(0)):
-        # Walk up all directories while there is an __init__ file
+    # ----------------------------------------------------------------------
+    def CreatePackageName():
+        # Continue traversing parent dirs as long as there is an __init__.py file.
         name_parts = []
 
-        directory = original_dir
+        directory = os.path.dirname(os.path.realpath(mod.__file__))
         while os.path.isfile(os.path.join(directory, "__init__.py")):
             directory, name = os.path.split(directory)
             name_parts.append(name)
 
         if not name_parts:
-            # If we didn't find any __init__ files, it means that this isn't a
-            # file that is part of (any) package. Hoever, we still want to simulate
-            # that it is part of a package so that relative imports work as 
-            # expected.
+            # If we didn't find any __init__ files, it means that this isn't a file
+            # that is part of a package. However, we still want to simulate package
+            # behavior so that relative imports work as expected.
             if name == "__main__" or getattr(sys, "frozen", False):
                 name = "___EntryPoint___"
             else:
@@ -99,65 +85,38 @@ def CreatePackageName(package, name, file):
             if fully_qualified_name not in sys.modules:
                 # When we load this module, it will be loaded under 'name_part'.
                 # Preserve the original module (if it exists).
-                if name_part in sys.modules:
-                    original_mod = sys.modules.pop(name_part)
-
-                    # ----------------------------------------------------------------------
-                    def Cleanup():
-                        sys.modules[name_part] = original_mod
-
-                    # ----------------------------------------------------------------------
+                temporary_modules[name_part] = sys.modules.pop(name_part, None)
                 
-                elif index != 0:
-                    # ----------------------------------------------------------------------
-                    def Cleanup():
-                        sys.modules[fully_qualified_name] = sys.modules[name_part]
-                        del sys.modules[name_part]
+                sys.path.insert(0, directory)
+                with CallOnExit(lambda: sys.path.pop(0)):
+                    # This will add the module name to sys.modules
+                    __import__(name_part)
 
-                    # ----------------------------------------------------------------------
-
-                else:
-                    # ----------------------------------------------------------------------
-                    def Cleanup():
-                        # Nothing to do here; keep the module
-                        pass
-
-                    # ----------------------------------------------------------------------
-
-                with CallOnExit(Cleanup):
-                    # Load this part
-                    sys.path.insert(0, directory)
-                    with CallOnExit(lambda: sys.path.pop(0)):
-                        # Importing a module will add its name to sys.modules
-                        mod = __import__(name_part)
-
+                sys.modules[fully_qualified_name] = sys.modules[name_part]
+                
             directory = os.path.join(directory, name_part)
 
         return fully_qualified_name
 
-# ----------------------------------------------------------------------
-@contextmanager
-def ApplyRelativePackage():
-    """\
-    Temporarily applies a new package name.
-
-    Example:
-        with ApplyRelativePackage():
-            from .Here.Is.A.Relative.Import import File
-    """
-
-    frame = inspect.stack()[2][0]           # 1 for ctor, 1 for contextmanager
-    mod = inspect.getmodule(frame)
-
-    original_value = frame.f_locals["__package__"]
+    # ----------------------------------------------------------------------
     
-    frame.f_locals["__package__"] = CreatePackageName(original_value, mod.__name__, mod.__file__)
+    original_package_name = frame.f_locals["__package__"]
 
     # ----------------------------------------------------------------------
     def Restore():
-        frame.f_locals["__package__"] = original_value
+        frame.f_locals["__package__"] = original_package_name
+
+        for k, v in six.iteritems(temporary_modules):
+            if v is None:
+                sys.modules.pop(k)
+            else:
+                sys.modules[k] = v
 
     # ----------------------------------------------------------------------
 
+    frame.f_locals["__package__"] = CreatePackageName()
+
     with CallOnExit(Restore):
         yield
+
+
