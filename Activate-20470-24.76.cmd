3ceb420d31242bdbe267ce61fbeabcff45260fd4
David Brownell <db@DavidBrownell.com>
2019-02-23 22:16:55 -0800

    Added a number of different libraries for Linux and python2

---
+++ Activate-20470-24.76.cmd
@@ -0,0 +1,4 @@
+echo. && echo. && echo ERROR: ^[WinError 5^] Access is denied: 'C:Codev3CommonEnvironmentExGeneratedWindowspython27Python' -^> 'C:Codev3CommonEnvironmentExGeneratedWindowspython27Python_ToDelete0'
+
+
+exit /B -1
