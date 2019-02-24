89755ac5d73fd3e2324977fa66dacd94bf5cb323
David Brownell <db@DavidBrownell.com>
2019-02-23 22:40:44 -0800

    Removed files incorrectly checked in

--- Activate-20470-24.76.cmd
+++
@@ -1,4 +0,0 @@
-echo. && echo. && echo ERROR: ^[WinError 5^] Access is denied: 'C:Codev3CommonEnvironmentExGeneratedWindowspython27Python' -^> 'C:Codev3CommonEnvironmentExGeneratedWindowspython27Python_ToDelete0'
-
-
-exit /B -1
