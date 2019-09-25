8b28075c91e95f3b14280295c5b448ad78e92682
David Brownell <db@DavidBrownell.com>
2019-09-24 21:57:28 -0700

    Updated repo url

--- Setup_custom.py
+++ Setup_custom.py
@@ -1,24 +1,24 @@
 # ----------------------------------------------------------------------
-# |  
+# |
 # |  Setup_custom.py
-# |  
+# |
 # |  David Brownell <db@DavidBrownell.com>
 # |      2018-05-03 22:12:13
-# |  
+# |
 # ----------------------------------------------------------------------
-# |  
+# |
 # |  Copyright David Brownell 2018-19.
 # |  Distributed under the Boost Software License, Version 1.0.
 # |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-# |  
+# |
 # ----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
-# |  
+# |
 # |  To setup an environment, run:
-# | 
+# |
 # |     Setup(.cmd|.ps1|.sh) [/debug] [/verbose] [/configuration=<config_name>]*
-# |  
+# |
 # ----------------------------------------------------------------------
 
 import os
@@ -29,8 +29,8 @@ from collections import OrderedDict
 import CommonEnvironment
 
 # ----------------------------------------------------------------------
-_script_fullpath = CommonEnvironment.ThisFullpath()
-_script_dir, _script_name = os.path.split(_script_fullpath)
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
 # ----------------------------------------------------------------------
 
 # <Missing function docstring> pylint: disable = C0111
@@ -40,13 +40,14 @@ _script_dir, _script_name = os.path.split(_script_fullpath)
 # <Wildcard import> pylint: disable = W0401
 # <Unused argument> pylint: disable = W0613
 
-fundamental_repo = os.getenv("DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL")
+fundamental_repo                            = os.getenv("DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL")
 assert os.path.isdir(fundamental_repo), fundamental_repo
 
 sys.path.insert(0, fundamental_repo)
 from RepositoryBootstrap import *                                           # <Unused import> pylint: disable = W0614
 from RepositoryBootstrap.SetupAndActivate import CurrentShell               # <Unused import> pylint: disable = W0614
 from RepositoryBootstrap.SetupAndActivate.Configuration import *            # <Unused import> pylint: disable = W0614
+
 del sys.path[0]
 
 # ----------------------------------------------------------------------
@@ -54,14 +55,14 @@ del sys.path[0]
 # repository may be activated within an environment at a time while any number
 # of mixin repositories can be activated within a standard repository environment.
 # Standard repositories may be dependent on other repositories (thereby inheriting
-# their functionality), support multiple configurations, and specify version 
+# their functionality), support multiple configurations, and specify version
 # information for tools and libraries in themselves or its dependencies.
 #
 # Mixin repositories are designed to augment other repositories. They cannot
-# have configurations or dependencies and may not be activated on their own. 
-# 
-# These difference are summarized in this table: 
-# 
+# have configurations or dependencies and may not be activated on their own.
+#
+# These difference are summarized in this table:
+#
 #                                                       Standard  Mixin
 #                                                       --------  -----
 #      Can be activated in isolation                       X
@@ -71,10 +72,10 @@ del sys.path[0]
 #      Can be activated within any other Standard                  X
 #        repository
 #
-# Consider a script that wraps common Git commands. This functionality is useful 
-# across a number of different repositories, yet doesn't have functionality that 
-# is useful on its own; it provides functionality that augments other repositories. 
-# This functionality should be included within a repository that is classified 
+# Consider a script that wraps common Git commands. This functionality is useful
+# across a number of different repositories, yet doesn't have functionality that
+# is useful on its own; it provides functionality that augments other repositories.
+# This functionality should be included within a repository that is classified
 # as a mixin repository.
 #
 # To classify a repository as a Mixin repository, decorate the GetDependencies method
@@ -93,35 +94,41 @@ def GetDependencies():
 
     configurations = OrderedDict()
 
-    configurations["python36"] = Configuration( "Python v3.6.5",
-                                                [ Dependency( "0EAA1DCF22804F90AD9F5A3B85A5D706",
-                                                              "Common_Environment",
-                                                              "python36",
-                                                              lambda scm_or_none: "https://github.com/davidbrownell/Common_Environment-v3.git" if scm_or_none is None or scm_or_none.Name != "Mercurial" else "ssh://{mercurial_ssh_config_name}/d:/Mercurial/Code/v3/Common/Common_Environment",
-                                                            ),
-                                                ],
-                                              )
+    configurations["python36"] = Configuration(
+        "Python v3.6.5",
+        [
+            Dependency(
+                "0EAA1DCF22804F90AD9F5A3B85A5D706",
+                "Common_Environment",
+                "python36",
+                "https://github.com/davidbrownell/Common_Environment-v3.git",
+            ),
+        ],
+    )
 
-    configurations["python27"] = Configuration( "Python v2.7.14",
-                                                [ Dependency( "0EAA1DCF22804F90AD9F5A3B85A5D706",
-                                                              "Common_Environment",
-                                                              "python27",
-                                                              lambda scm_or_none: "https://github.com/davidbrownell/Common_Environment-v3.git" if scm_or_none is None or scm_or_none.Name != "Mercurial" else "ssh://{mercurial_ssh_config_name}/d:/Mercurial/Code/v3/Common/Common_Environment",
-                                                            ),
-                                                ],
-                                              )
+    configurations["python27"] = Configuration(
+        "Python v2.7.14",
+        [
+            Dependency(
+                "0EAA1DCF22804F90AD9F5A3B85A5D706",
+                "Common_Environment",
+                "python27",
+                "https://github.com/davidbrownell/Common_Environment-v3.git",
+            ),
+        ],
+    )
     return configurations
+
 
 # ----------------------------------------------------------------------
 def GetCustomActions(debug, verbose, explicit_configurations):
     """
     Returns an action or list of actions that should be invoked as part of the setup process.
 
-    Actions are generic command line statements defined in 
+    Actions are generic command line statements defined in
     <Common_Environment>/Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Shell/Commands/__init__.py
     that are converted into statements appropriate for the current scripting language (in most
     cases, this is Bash on Linux systems and Batch or Powershell on Windows systems.
     """
-    
+
     return []
-    
